22f09b7d45c3df86c39b8cb7daab31f36f296508
Daria Ropka <daria@Daria-Rejdych-MBP.local>
2016-07-03 19:44:33 +0200

    checkbox

--- js/app.js
+++ js/app.js
@@ -2,16 +2,14 @@ document.addEventListener("DOMContentLoaded", function() {
     var mainElements = document.querySelector("nav > ul").children;
     for (var i = 0; i < mainElements.length; i++) {
         mainElements[i].addEventListener("mouseover", function() {
-            console.log("najechane");
+
             var innerUl = this.querySelector("ul");
-            console.log("innerUl" + innerUl);
+
             if (innerUl !== null) {
-                console.log("if");
                 innerUl.style.display = "block";
             }
-        })
+        });
         mainElements[i].addEventListener("mouseout", function() {
-            console.log("zjechane");
             var innerUl = this.querySelector("ul");
             if (innerUl !== null) {
                 innerUl.style.display = "none";
@@ -25,27 +23,31 @@ document.addEventListener("DOMContentLoaded", function() {
     var tab = ["images/black_chair.png", "images/orange.png"];
     var counter = 0;
     var setImage = function() {
-      console.log("name " + counter);
         imgChair.src = tab[counter];
     };
-setImage();
+
+    setImage();
+
     prevBtn.addEventListener("click", function() {
-      if (counter <= 0) {
-          counter = tab.length -1;
-      }else {
-          counter--;
-      }
+
+        if (counter <= 0) {
+            counter = tab.length - 1;
+        } else {
+            counter--;
+        }
+
         setImage()
+
     });
 
     var nextBtn = document.querySelector(".next");
     nextBtn.addEventListener("click", function() {
 
-      if (counter >= tab.length - 1) {
-          counter = 0;
-      }else {
-          counter++;
-      }
+        if (counter >= tab.length - 1) {
+            counter = 0;
+        } else {
+            counter++;
+        }
 
         setImage();
 
